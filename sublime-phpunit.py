7655c15bf4165de96635022400398b58c0c7d0c4
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2025-02-09 07:32:07 -0600

    Support Pest PHP
    Experimental support for Pest PHP.
    
    Used ChatGPT to support Pest PHP.

--- sublime-phpunit.py
+++ sublime-phpunit.py
@@ -3,6 +3,7 @@ import sys
 import shlex
 import ntpath
 import subprocess
+import re
 import sublime
 import sublime_plugin
 
@@ -40,32 +41,58 @@ class PhpunitTestCommand(sublime_plugin.WindowCommand):
         for r in reversed(function_regions):
             if r.a < sel.a:
                 cf = view.substr(r)
+                self.is_pest_test = False
                 break
+
+        if cf is None:
+            # Try to get Pest test description
+            cf = self.get_current_pest_test_name(view, sel.a)
+            if cf:
+                self.is_pest_test = True
+            else:
+                self.is_pest_test = False
+
         return cf
+
+    def get_current_pest_test_name(self, view, position):
+        pattern = r"test\s*\(\s*['\"](.*?)['\"]"
+        pest_tests = view.find_all(pattern)
+        for r in reversed(pest_tests):
+            if r.a < position:
+                line_region = view.line(r)
+                line_text = view.substr(line_region)
+                test_match = re.search(pattern, line_text)
+                if test_match:
+                    return test_match.group(1)
+        return None
 
     def find_phpunit_config(self, file_name):
         phpunit_config_path = file_name
         found = False
-        while found == False:
+        while not found:
             phpunit_config_path = os.path.abspath(os.path.join(phpunit_config_path, os.pardir))
-            found = os.path.isfile(phpunit_config_path + '/phpunit.xml') or os.path.isfile(phpunit_config_path + '/phpunit.xml.dist') or phpunit_config_path == '/'
+            found = os.path.isfile(os.path.join(phpunit_config_path, 'phpunit.xml')) or \
+                    os.path.isfile(os.path.join(phpunit_config_path, 'phpunit.xml.dist')) or \
+                    phpunit_config_path == '/'
+            if phpunit_config_path == '/':
+                break
         return phpunit_config_path
 
     def find_phpunit_bin(self, directory):
         search_paths = [
+            'vendor/bin/pest',
             'vendor/bin/phpunit',
             'vendor/bin/phpunit/phpunit/phpunit',
         ]
 
-        found = False;
+        found = False
         for path in search_paths:
-            if False == found:
-                binpath = os.path.realpath(directory + "/" + path)
+            binpath = os.path.realpath(os.path.join(directory, path))
+            if os.path.isfile(binpath):
+                found = True
+                break
 
-                if os.path.isfile(binpath.replace("\\", "")):
-                    found = True
-
-        if False == found:
+        if not found:
             binpath = 'phpunit'
 
         return binpath
@@ -89,15 +116,14 @@ class RunPhpunitTestCommand(PhpunitTestCommand):
     def run(self, *args, **kwargs):
         file_name, phpunit_config_path, phpunit_bin, active_view, directory = self.get_paths()
 
-        self.run_in_terminal('cd ' + phpunit_config_path + self.get_cmd_connector() + phpunit_bin + ' ' + file_name)
+        self.run_in_terminal('cd ' + shlex.quote(phpunit_config_path) + self.get_cmd_connector() + shlex.quote(phpunit_bin) + ' ' + shlex.quote(file_name))
 
 class RunAllPhpunitTestsCommand(PhpunitTestCommand):
 
     def run(self, *args, **kwargs):
         file_name, phpunit_config_path, phpunit_bin, active_view, directory = self.get_paths()
 
-        self.run_in_terminal('cd ' + phpunit_config_path + self.get_cmd_connector() + phpunit_bin)
-
+        self.run_in_terminal('cd ' + shlex.quote(phpunit_config_path) + self.get_cmd_connector() + shlex.quote(phpunit_bin))
 
 class RunSinglePhpunitTestCommand(PhpunitTestCommand):
 
@@ -105,8 +131,17 @@ class RunSinglePhpunitTestCommand(PhpunitTestCommand):
         file_name, phpunit_config_path, phpunit_bin, active_view, directory = self.get_paths()
 
         current_function = self.get_current_function(active_view)
+        if not current_function:
+            sublime.error_message("Could not determine the current test.")
+            return
 
-        self.run_in_terminal('cd ' + phpunit_config_path + self.get_cmd_connector() + phpunit_bin + ' ' + file_name + " --filter '/::" + current_function + "$/'")
+        if self.is_pest_test:
+            filter_param = shlex.quote(current_function)
+        else:
+            filter_param = shlex.quote('/::' + current_function + '$/')
+
+        command = 'cd ' + shlex.quote(phpunit_config_path) + self.get_cmd_connector() + shlex.quote(phpunit_bin) + ' ' + shlex.quote(file_name) + ' --filter ' + filter_param
+        self.run_in_terminal(command)
 
 class RunLastPhpunitTestCommand(PhpunitTestCommand):
 
@@ -114,7 +149,7 @@ class RunLastPhpunitTestCommand(PhpunitTestCommand):
         file_name, phpunit_config_path, phpunit_bin, active_view, directory = self.get_paths()
 
         if 'Test' in file_name:
-            RunSinglePhpunitTestCommand.run(self, args, kwargs);
+            RunSinglePhpunitTestCommand.run(self, *args, **kwargs)
         elif self.lastTestCommand:
             self.run_in_terminal(self.lastTestCommand)
 
@@ -123,31 +158,36 @@ class RunPhpunitTestsInDirCommand(PhpunitTestCommand):
     def run(self, *args, **kwargs):
         file_name, phpunit_config_path, phpunit_bin, active_view, directory = self.get_paths()
 
-        self.run_in_terminal('cd ' + phpunit_config_path + self.get_cmd_connector() + phpunit_bin + ' ' + directory)
+        self.run_in_terminal('cd ' + shlex.quote(phpunit_config_path) + self.get_cmd_connector() + shlex.quote(phpunit_bin) + ' ' + shlex.quote(directory))
 
 class RunSingleDuskTestCommand(PhpunitTestCommand):
 
     def run(self, *args, **kwargs):
-        file_name, phpunit_config_path, active_view, directory = self.get_paths()
+        file_name, phpunit_config_path, phpunit_bin, active_view, directory = self.get_paths()
 
         current_function = self.get_current_function(active_view)
+        if not current_function:
+            sublime.error_message("Could not determine the current test.")
+            return
 
-        self.run_in_terminal('cd ' + phpunit_config_path + self.get_cmd_connector() + 'php artisan dusk ' + file_name + ' --filter ' + current_function)
+        command = 'cd ' + shlex.quote(phpunit_config_path) + self.get_cmd_connector() + 'php artisan dusk ' + shlex.quote(file_name) + ' --filter ' + shlex.quote(current_function)
+        self.run_in_terminal(command)
 
 class RunAllDuskTestsCommand(PhpunitTestCommand):
 
     def run(self, *args, **kwargs):
-        file_name, phpunit_config_path, active_view, directory = self.get_paths()
+        file_name, phpunit_config_path, phpunit_bin, active_view, directory = self.get_paths()
 
-        self.run_in_terminal('cd ' + phpunit_config_path + self.get_cmd_connector() + 'php artisan dusk')
+        command = 'cd ' + shlex.quote(phpunit_config_path) + self.get_cmd_connector() + 'php artisan dusk'
+        self.run_in_terminal(command)
 
 class RunDuskTestsInDirCommand(PhpunitTestCommand):
 
     def run(self, *args, **kwargs):
-        file_name, phpunit_config_path, active_view, directory = self.get_paths()
+        file_name, phpunit_config_path, phpunit_bin, active_view, directory = self.get_paths()
 
-        self.run_in_terminal('cd ' + phpunit_config_path + self.get_cmd_connector() + 'php artisan dusk ' + directory)
-
+        command = 'cd ' + shlex.quote(phpunit_config_path) + self.get_cmd_connector() + 'php artisan dusk ' + shlex.quote(directory)
+        self.run_in_terminal(command)
 
 class FindMatchingTestCommand(sublime_plugin.WindowCommand):
 
@@ -170,9 +210,17 @@ class FindMatchingTestCommand(sublime_plugin.WindowCommand):
 
         # Big dirty macro-ish hack. Eventually I should just open the file in some sort of
         # logical way.
-        self.window.run_command("set_layout", {"cells": [[0, 0, 1, 1], [1, 0, 2, 1]], "cols": [0.0, 0.5, 1.0], "rows": [0.0, 1.0]})
+        self.window.run_command("set_layout", {
+            "cells": [[0, 0, 1, 1], [1, 0, 2, 1]],
+            "cols": [0.0, 0.5, 1.0],
+            "rows": [0.0, 1.0]
+        })
         self.window.run_command("focus_group", {"group": tab_target})
-        self.window.run_command("show_overlay", {"overlay": "goto", "text": file_name, "show_files": "true"})
+        self.window.run_command("show_overlay", {
+            "overlay": "goto",
+            "text": file_name,
+            "show_files": "true"
+        })
         self.window.run_command("move", {"by": "lines", "forward": False})
 
         # This is a dirty hack to get it to switch files... Can't simulate 'Enter'
@@ -180,4 +228,3 @@ class FindMatchingTestCommand(sublime_plugin.WindowCommand):
         self.window.run_command("show_overlay", {"overlay": "goto", "show_files": "true"})
         self.window.run_command("focus_group", {"group": 0})
         self.window.run_command("focus_group", {"group": tab_target})
-
